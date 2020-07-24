9d53e8f5fc43dcc51a28800f395154d341b3a39b
jelneo <neohuilingjelena@gmail.com>
2020-07-24 11:13:40 +0800

    Switch to multiline quotes

--- dbhelper.py
+++ dbhelper.py
@@ -13,22 +13,22 @@ class DBHelper:
         self.table = config['db_table']
 
     def setup(self):
-        statement = f"CREATE TABLE IF NOT EXISTS {self.table} (logID INTEGER PRIMARY KEY, timestamp text, description text)"
+        statement = f"""CREATE TABLE IF NOT EXISTS {self.table} (logID INTEGER PRIMARY KEY, timestamp text, description text)"""
         self.conn.execute(statement)
         self.conn.commit()
 
     def add_log(self, timestamp, description):
-        statement = f"INSERT INTO {self.table} (timestamp, description) VALUES (?, ?)"
-        args = (str(timestamp), description,)
+        statement = f"""INSERT INTO {self.table} (timestamp, description) VALUES (?, ?)"""
+        args = (str(timestamp), str(description),)
         self.conn.execute(statement, args)
         self.conn.commit()
 
     def delete_log(self, timestamp):
-        statement = f"DELETE FROM {self.table} WHERE timestamp = (?)"
-        args = (timestamp,)
+        statement = f"""DELETE FROM {self.table} WHERE timestamp = (?)"""
+        args = (str(timestamp),)
         self.conn.execute(statement, args)
         self.conn.commit()
 
     def get_latest_log(self):
-        statement = f"SELECT timestamp, description FROM {self.table} WHERE logID = (SELECT MAX(logID) FROM {self.table})"
+        statement = f"""SELECT timestamp, description FROM {self.table} WHERE logID = (SELECT MAX(logID) FROM {self.table})"""
         return [DELIMITER.join(x) for x in self.conn.execute(statement)]
